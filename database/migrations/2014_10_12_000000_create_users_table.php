5424deeca0f51280499d71766ffd196bdb67a17c
bondaa123 <zackiealvian28@gmail.com>
2025-02-26 10:29:54 +0700

    Membuat Model dan Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,17 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('image');
+        $table->string('title');
+        $table->text('description');
+        $table->bigInteger('price');
+        $table->integer('stock')->default(0);
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
